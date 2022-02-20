e34fe7e84fbfe77ec50a4fb58f29866c3b7f0239
Dipesh <myselfdipu69@gmail.com>
2022-02-20 22:08:33 +0530

    login screen

---
+++ src/screens/login.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text, Stylesheet} from 'react-native';
+
+const LoginScreen = () => {
+	return(
+	<View style={styles.screen>
+
+		</View>
+	)
+};
+
+const styles = Stylesheet.create({
+	screen: {
+		flex: 1,
+		backgroundColor: 'transparent',
+	},
+	loginScreen: {
+	},
+});
